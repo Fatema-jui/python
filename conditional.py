ac378adf5617da6d07ec3b423f0e7d1fb121c36e
Fatema-jui <asratulmontaha@gmail.com>
2026-09-01 16:48:27 +0600

    conditional statement

---
+++ conditional.py
@@ -0,0 +1,23 @@
+rain = 1
+if rain==1:
+    print("It is raining")
+else:
+    print("It's not raining")   
+
+
+#check number is positive, negative or zero
+num = int(input("Enter a number"))
+if num > 0:
+    print("Number is positive")
+elif num == 0:
+    print("Number is zero")
+else:
+    print("Number is negative")     
+
+num = 5
+if num>0:
+    print("number is greater than zero") 
+elif num%2==0:
+    print("number is even")
+else:
+    print("number is odd")           
